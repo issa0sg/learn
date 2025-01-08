e76265a10ca65474ea90ce8dfdb8e39a349030cb
sagi.issa <sagi.issa@freedompay.money>
2025-01-08 13:42:56 +0500

    ura we can do it

--- public/index.php
+++ public/index.php
@@ -5,19 +5,38 @@ var_dump($_REQUEST, $_SERVER);
 echo '<h1>Hello world</h1>';
 
 $filename = 'example.txt';
+$filename2 = 'file.txt';
 $content = "Привет, мир! Это пример записи в файл.\n";
 
+
+echo "Current user: " . exec('whoami');
+echo "\n";
+echo "Current group: " . exec('groups');
+
 // Открываем файл для записи ('w') или создания, если он не существует
-$file = fopen($filename, 'w');
+$file = fopen($filename, 'aw');
+$file2 = fopen($filename2, 'aw');
 
 if ($file) {
     // Пишем содержимое в файл
     fwrite($file, $content);
-    
+
     // Закрываем файл
     fclose($file);
-    
+
     echo "Файл '$filename' успешно создан и данные записаны.";
 } else {
     echo "Не удалось открыть файл '$filename' для записи.";
 }
+
+    if ($file2) {
+        // Пишем содержимое в файл
+        fwrite($file2, $content);
+
+        // Закрываем файл
+        fclose($file2);
+
+        echo "Файл '$filename2' успешно создан и данные записаны.";
+    } else {
+        echo "Не удалось открыть файл '$filename2' для записи.";
+    }
